ca8a806937ac264e0421a8aad7d4f375b685f62e
Adrian Parker <iamhiddensomewhere@gmail.com>
2016-04-05 17:13:43 -0400

    Add missing app instantiation.
    Expose /static as the root.

--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
 'use strict'
 const express = require('express')
 
+const app = express()
 
 app.use('/node_modules', express.static('./node_modules'))
 app.use('/build', express.static('./build'))
-app.use('/static', express.static('./static'))
+app.use('/', express.static('./static'))
 
 app.listen(80, () => console.log('Listening on 80'))
 
