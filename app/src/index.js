d4dc9dc266765abdb313462edfa804afe9ba6db1
Adrian Parker <iamhiddensomewhere@gmail.com>
2016-04-11 21:52:46 -0400

    Start the store and reducers.

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { Provider } from 'react-redux'
 import App from './containers/App'
+import configureStore from './store/configureStore'
 
-ReactDom.render(<App />, document.getElementById('mount-point'))
+const store = configureStore()
+
+ReactDom.render(<Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('mount-point'))
