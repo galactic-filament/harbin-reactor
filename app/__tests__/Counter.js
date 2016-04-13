d49f2106f699af299c11f9d965b09408e809e4de
Adrian Parker <iamhiddensomewhere@gmail.com>
2016-04-13 12:13:11 -0400

    Fix Counter component test.

--- app/__tests__/Counter.js
+++ app/__tests__/Counter.js
@@ -1,16 +1,35 @@
-/*global describe it expect*/
+/*global describe it*/
 
+import expect from 'expect'
 import React from 'react'
 import TestUtils from 'react-addons-test-utils'
 import Counter from '../src/components/Counter'
 
+const setup = (propOverrides) => {
+  const props = Object.assign({
+    count: 0,
+    actions: {
+      incrementCount: expect.createSpy()
+    }
+  }, propOverrides)
+  const renderer = TestUtils.createRenderer()
+  renderer.render(<Counter {...props} />)
+
+  const output = renderer.getRenderOutput()
+
+  return {
+    props: props,
+    output: output,
+    renderer: renderer
+  }
+}
+
 describe('Counter', () => {
   it('adds', () => {
-    const initialCount = 0
-    const counter = TestUtils.renderIntoDocument(
-      <Counter initialCount={initialCount} />
-    )
-    counter.handleClick()
-    expect(counter.state.count).toEqual(initialCount + 1)
+    const { props, output } = setup()
+    const [ , button ] = output.props.children
+    
+    button.props.onClick()
+    expect(props.actions.incrementCount).toHaveBeenCalled()
   })
 })
