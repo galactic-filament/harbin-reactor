81708389455b384103b10558a6f096fca7eb15f8
Adrian Parker <iamhiddensomewhere@gmail.com>
2017-09-02 14:03:09 -0400

    Start typescript-based react app.

--- app/src/store/configureStore.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import rootReducer from '../reducers'
-
-export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState, applyMiddleware(thunkMiddleware))
-
-  return store
-}
