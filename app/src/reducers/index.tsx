8c008827806c54ab55353f2b73723b963f17d1aa
Adrian Parker <iamhiddensomewhere@gmail.com>
2017-10-09 17:46:33 -0400

    Fix linting errors.

--- app/src/reducers/index.tsx
+++ app/src/reducers/index.tsx
@@ -5,10 +5,10 @@ import { INCREMENT_ENTHUSIASM, DECREMENT_ENTHUSIASM } from '../constants/index';
 export function enthusiasm(state: StoreState, action: EnthusiasmAction): StoreState {
     switch (action.type) {
         case INCREMENT_ENTHUSIASM:
-            return { ...state, enthusiasmLevel: state.enthusiasmLevel+1 };
+            return { ...state, enthusiasmLevel: state.enthusiasmLevel + 1 };
         case DECREMENT_ENTHUSIASM:
-            return { ...state, enthusiasmLevel: state.enthusiasmLevel-1 };
+            return { ...state, enthusiasmLevel: state.enthusiasmLevel - 1 };
+        default:
+            return state;
     }
-
-    return state;
 }
