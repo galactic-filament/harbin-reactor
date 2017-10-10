94f788e80dc4a834962e844e4b8a045422fd5f2f
Adrian Parker <iamhiddensomewhere@gmail.com>
2017-10-09 20:38:21 -0400

    Add test for verifying error throwing when enthusiasmLevel is zero.

--- app/src/components/Hello.test.tsx
+++ app/src/components/Hello.test.tsx
@@ -1,13 +1,23 @@
 import * as React from 'react';
-import * as enzyme from 'enzyme';
+import { configure, shallow } from 'enzyme';
 
 const Adapter = require('enzyme-adapter-react-15');
 
 import Hello from './Hello';
 
-enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
 
 it('Renders correctly when no enthusiasm level is given', () => {
-    const hello = enzyme.shallow(<Hello name="Adrian" />);
+    const hello = shallow(<Hello name="Adrian" />);
     expect(hello.find('.greeting').text()).toEqual('Hello Adrian!');
+});
+
+it('Renders correctly when 1 enthusiasm level is given', () => {
+    const hello = shallow(<Hello name="Adrian" enthusiasmLevel={2} />);
+    expect(hello.find('.greeting').text()).toEqual('Hello Adrian!!');
+});
+
+it('Renders correctly when 1 enthusiasm level is given', () => {
+    expect(() => shallow(<Hello name="Adrian" enthusiasmLevel={0} />))
+        .toThrowError('You could be more enthusiastic!');
 });
