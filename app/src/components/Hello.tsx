052fa115db14058885f802c0842188501c65d805
Adrian Parker <iamhiddensomewhere@gmail.com>
2017-12-04 16:32:18 -0500

    Add on-increment/on-decrement props to hello component.

--- app/src/components/Hello.tsx
+++ app/src/components/Hello.tsx
@@ -5,9 +5,11 @@ import './Hello.css';
 export interface Props {
   name: string;
   enthusiasmLevel?: number;
+  onDecrement?: () => void;
+  onIncrement?: () => void;
 }
 
-function Hello({ name, enthusiasmLevel = 1 }: Props) {
+function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
   if (enthusiasmLevel <= 0) {
     throw new Error('You could be more enthusiastic!');
   }
@@ -16,6 +18,10 @@ function Hello({ name, enthusiasmLevel = 1 }: Props) {
     <div className="hello">
       <div className="greeting">
         Hello {name + getExcalamationMarks(enthusiasmLevel)}
+        <div>
+          <button onClick={onDecrement}>-</button>
+          <button onClick={onIncrement}>+</button>
+        </div>
       </div>
     </div>
   );
